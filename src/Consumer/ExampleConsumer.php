e64fec410bdd1895b10b193fbd543dfef28f92b2
Carlos Belisario <cbelisario_ext@dlatv.net>
2017-01-10 16:31:29 -0300

    new: example consumer

---
+++ src/Consumer/ExampleConsumer.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace PlayingWihtRabbitMq\Consumer;
+
+use PhpAmqpLib\Message\AMQPMessage;
+
+/**
+ * Class ExampleConsumer
+ * @author Carlos Belisario <carlos.belisario.gonzalez@gmail.com>
+ */
+class ExampleConsumer extends Consumer
+{
+    /**
+     * example of consumer queue, put the logic in the process method
+     * @param AMQPMessage $message
+     */
+    public function process(AMQPMessage $message)
+    {
+        echo " [x] Received ", $message->body, "\n";
+        sleep(10);
+        echo " [x] Done", "\n";
+        $message->delivery_info['channel']->basic_ack($message->delivery_info['delivery_tag']);
+    }
+}
+
